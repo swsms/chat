ec0f85654bbb49b1a9691478c13cc3673ef34830
art <swsms@mail.ru>
2019-01-08 16:53:07 +0500

    Configure pom's to generate uber-JAR's

---
+++ client/src/main/java/org/artb/chat/client/ClientRunner.java
@@ -0,0 +1,12 @@
+package org.artb.chat.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ClientRunner {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClientRunner.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Client started");
+    }
+}
---
+++ server/src/main/java/org/artb/chat/server/ServerRunner.java
@@ -0,0 +1,12 @@
+package org.artb.chat.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ServerRunner {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServerRunner.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Server started");
+    }
+}
