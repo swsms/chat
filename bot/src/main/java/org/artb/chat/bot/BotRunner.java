62830761234df1e910160d06108e60c8cb0c1c1e
art <swsms@mail.ru>
2019-01-16 21:24:39 +0500

    Add a separated module for bots

---
+++ bot/src/main/java/org/artb/chat/bot/BotRunner.java
@@ -0,0 +1,12 @@
+package org.artb.chat.bot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BotRunner {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BotRunner.class);
+
+    public static void main(String[] args)  {
+        LOGGER.info("All bots successfully started.");
+    }
+}
